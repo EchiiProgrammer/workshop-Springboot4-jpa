33fe27d5c7e2f38479cc8de999bc92b69b6c560e
Geovane Pires <piresgamer@gmail.com>
2024-02-15 19:11:21 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,13 +1,16 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 
 @Entity
@@ -20,6 +23,8 @@ public class Category implements Serializable{
 	private Long id;
 	private String name;
 	
+	@Transient
+	private Set<Product> products =  new HashSet<>();
 	
 	public Category() {
 		
@@ -52,6 +57,9 @@ public class Category implements Serializable{
 		this.name = name;
 	}
 
+	public Set<Product> getProducts() {
+		return products;
+	}
 
 	@Override
 	public int hashCode() {
@@ -70,6 +78,9 @@ public class Category implements Serializable{
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
+	
 
 
 	
