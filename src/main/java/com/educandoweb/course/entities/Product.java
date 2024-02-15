2392ff21c37fdf4d90ac657ec945375644ba532b
Geovane Pires <piresgamer@gmail.com>
2024-02-15 19:46:19 -0300

    Many-To-Many association with Join Table

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -9,15 +9,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -25,88 +26,83 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
-	
-public Product() {
-	
-}
 
-public Product(Long id, String name, String description, Double price, String imgUrl) {
-	super();
-	this.id = id;
-	this.name = name;
-	this.description = description;
-	this.price = price;
-	this.imgUrl = imgUrl;
-}
+	public Product() {
 
-public Long getId() {
-	return id;
-}
+	}
 
-public void setId(Long id) {
-	this.id = id;
-}
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+	}
 
-public String getName() {
-	return name;
-}
+	public Long getId() {
+		return id;
+	}
 
-public void setName(String name) {
-	this.name = name;
-}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-public String getDescription() {
-	return description;
-}
+	public String getName() {
+		return name;
+	}
 
-public void setDescription(String description) {
-	this.description = description;
-}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-public Double getPrice() {
-	return price;
-}
+	public String getDescription() {
+		return description;
+	}
 
-public void setPrice(Double price) {
-	this.price = price;
-}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-public String getImgUrl() {
-	return imgUrl;
-}
+	public Double getPrice() {
+		return price;
+	}
 
-public void setImgUrl(String imgUrl) {
-	this.imgUrl = imgUrl;
-}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 
-public Set<Category> getCategories() {
-	return categories;
-};
+	public String getImgUrl() {
+		return imgUrl;
+	}
 
-@Override
-public int hashCode() {
-	return Objects.hash(id);
-}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
 
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Product other = (Product) obj;
-	return Objects.equals(id, other.id);
-}
+	public Set<Category> getCategories() {
+		return categories;
+	};
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 
-
-
-
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
 
 }
